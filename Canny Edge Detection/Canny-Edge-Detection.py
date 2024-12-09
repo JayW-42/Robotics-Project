85f9d552d2a304b587f9d9d5d62376f93cee181c
JayW-42 <jay.warren@colorado.edu>
2024-12-08 23:07:25 -0700

    imageread

--- Canny Edge Detection/Canny-Edge-Detection.py
+++ Canny Edge Detection/Canny-Edge-Detection.py
@@ -1,1 +1,21 @@
-print("hello world")
+import numpy as np
+import cv2
+import glob
+import random
+import matplotlib.pyplot as plt
+from scipy import signal
+from numpy.linalg import svd, inv
+import os
+#from lolviz import matrixviz
+
+os.chdir(os.path.join(os.path.dirname(__file__)))
+
+print(r'driving.jpg' in os.listdir("."))
+img = cv2.imread(r'driving.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('driving',img)
+
+# Use a Box Filter to blur the image
+def noise_reduction(img):
+    blurred = cv2.blur(img,(4,4))
+    return blurred
+
